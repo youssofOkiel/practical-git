97f5b9c52c3802d9cebd3ece7917a895f01c373f
youssofOkiel <youssofokiel3@gmail.com>
2024-06-06 00:44:07 +0300

    added privacy route

--- routes/api/v1/client.php
+++ routes/api/v1/client.php
@@ -11,4 +11,8 @@ Route::prefix('api/v1/client')->group(function () {
     Route::post('/contact', function () {
         return "Hello from the API v1 Contact route!";
     });
+
+    Route::post('/privacy', function () {
+        return "Hello from the API v1 Privacy route!";
+    });
 });
