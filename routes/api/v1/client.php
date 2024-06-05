a6f64f9cc16cba8c102b46535ee6234741615cb1
youssofOkiel <youssofokiel3@gmail.com>
2024-06-06 01:29:46 +0300

    Squashed commit of the following:
    commit 7832f823ca69778c70e8e250aeb00cf9905464e2
    Author: youssofOkiel <youssofokiel3@gmail.com>
    Date:   Thu Jun 6 00:33:28 2024 +0300
    
        add api routes
    
        added about route
    
        added contact route
    
        added privacy route
    
        routes prifex
    
        added home route
    
        added about route
    
        added privacy route
    
    commit 97f5b9c52c3802d9cebd3ece7917a895f01c373f
    Author: youssofOkiel <youssofokiel3@gmail.com>
    Date:   Thu Jun 6 00:44:07 2024 +0300
    
        added privacy route
    
    commit eb20dc27ef50fa3444b56d945ab70fa219c92c5d
    Author: youssofOkiel <youssofokiel3@gmail.com>
    Date:   Thu Jun 6 00:43:32 2024 +0300
    
        added contact route
    
    commit 7dea7a1ff51f8614dd1a63911a67805d450882fb
    Author: youssofOkiel <youssofokiel3@gmail.com>
    Date:   Thu Jun 6 00:42:38 2024 +0300
    
        added about route
    
    commit cc6caf8c6385fba029c1da016c1386c0f9528cf7
    Author: youssofOkiel <youssofokiel3@gmail.com>
    Date:   Thu Jun 6 00:33:28 2024 +0300
    
        routes prifex

--- routes/api/v1/client.php
+++ routes/api/v1/client.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/client', function () {
-    return "Hello from the API v1 client route!";
+Route::prefix('api/v1/client')->group(function () {
+
+    Route::get('/about', function () {
+        return "Hello from the API v1 About route!";
+    });
+
+    Route::post('/contact', function () {
+        return "Hello from the API v1 Contact route!";
+    });
+
+    Route::post('/privacy', function () {
+        return "Hello from the API v1 Privacy route!";
+    });
 });
